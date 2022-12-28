b62bc4bccddd018786d7aaa2ac0f54e66d07d938
Doqso <p3lopubico@gmail.com>
2022-12-28 21:52:30 +0100

    some method improvements added

--- script.py
+++ script.py
@@ -2,12 +2,13 @@ import time
 from Cryptodome.Cipher import AES
 
 
-def get_16b_text(text: bytes) -> bytes:
+def get_16bytes_str(text: str) -> bytes:
     text_module = len(text) % 16
+    output = bytes(text, "utf-8")
     if text_module != 0:
         remaining_spaces = 16 - text_module
-        return bytes([*bytes(remaining_spaces), *text])
-    return text
+        return bytes([*bytes(remaining_spaces), *output])
+    return output
 
 
 def sleep(seconds):
@@ -17,8 +18,8 @@ def sleep(seconds):
     print()
 
 
-def encrypt(passwd_bytes: bytes, file_name: str):
-    key = get_16b_text(passwd_bytes)
+def encrypt(passwd: str, file_name: str):
+    key = get_16bytes_str(passwd)
     cipher = AES.new(key, AES.MODE_EAX)
     nonce = cipher.nonce
     with open(file_name, "r+b") as f:
@@ -31,9 +32,9 @@ def encrypt(passwd_bytes: bytes, file_name: str):
     print("Encryption done")
 
 
-def decrypt(passwd_bytes: bytes, file_name: str):
+def decrypt(passwd: str, file_name: str):
     with open(file_name, "r+b") as f:
-        key = get_16b_text(passwd_bytes)
+        key = get_16bytes_str(passwd)
         nonce = f.read(16)
         tag = f.read(16)
         cipher_text = f.read()
@@ -50,7 +51,7 @@ def decrypt(passwd_bytes: bytes, file_name: str):
 
 # -------------------------------- Main
 if __name__ == "__main__":
-    passwd = b'my_password'
+    passwd = 'my_password'
     file_name = "file_to_encrypt.exe"
     encrypt(passwd, file_name)
     sleep(2)
